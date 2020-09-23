560ebe2212df2ae0f06bc35fc720e926282c9e22
huynh như <nhubongxe@gmail.com>
2020-09-23 16:33:26 +0700

    page tình hình

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ import { TintucComponent } from './home/tintuc/tintuc.component';
 import { MainComponent } from './admin/layout/main/main.component';
 import { LoginComponent } from './admin/login/login.component';
 import { PostComponent } from './admin/post/post.component';
+import { TinhHinhComponent } from './home/tinh-hinh/tinh-hinh.component';
+import { PrecautionsComponent } from './home/precautions/precautions.component';
+import { SymptomsComponent } from './home/symptoms/symptoms.component';
 
 const routes: Routes = [
   {
@@ -23,7 +26,22 @@ const routes: Routes = [
       {
         path: 'tintuc',
         component: TintucComponent
-      }
+      },
+      {
+        path: 'tinh-hinh',
+        component: TinhHinhComponent,
+        children:[
+          {
+            path: "precautions",
+            component: PrecautionsComponent
+          },
+          {
+            path: "symptoms",
+            component: SymptomsComponent
+          },
+        ]
+      },
+      
     ]
   },
   {
